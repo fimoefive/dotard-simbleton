28c5950e8bc271b8688cd8206821b4fbdfb0f67f
fimoefive <brothermj@hotmail.com>
2020-09-24 23:56:52 -0500

    html formatting changes and import functions script file tag changes.

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,5 +1,5 @@
 
-export const BusinessHTML = (businessObject) => {
+export const businessHTML = (businessObject) => {
     return `
         <section class="business">
             <h2 class="company__name">${businessObject.companyName}</h2>
@@ -15,13 +15,13 @@ export const BusinessHTML = (businessObject) => {
 export const newYorkBusinessesHTML = (businessObj) => {
     return `
         <section class="nybusiness">
-        <h2 class="company__name">${businessObject.companyName}</h2>
-        <div class="company__address">
-            <p>Adress: ${businessObject.addressFullStreet}</p>
-            <p>${businessObject.addressCity}, ${businessObject.addressStateCode}</p> 
-            <p>${businessObject.addressZipCode}</p>
-        </div>
-    </section>
+            <h2 class="company__name">${businessObj.companyName}</h2>
+            <div class="company__address">
+                <p>Adress: ${businessObj.addressFullStreet}</p>
+                <p>${businessObj.addressCity}, ${businessObj.addressStateCode}</p> 
+                <p>${businessObj.addressZipCode}</p>
+            </div>
+        </section>
     `
 };
 
--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,20 +1,19 @@
 import { useBusinesses, purchaserAgent, findPurchaser } from "./BusinessProvider.js";
 import { newYorkBusiness, manufacturingBusinesses } from "./BusinessProvider.js";
-import { Business } from "./Business.js";
+import { businessHTML, newYorkBusinessesHTML, agentHTML } from "./Business.js";
+
 
 const contentTarget = document.querySelector(".business");
 const nyContentTarget = document.querySelector(".businessList--newYork");
 const agentContentTarget = document.querySelector(".agents");
 
-
-
-export const BusinessList = () => {
+export const businessList = () => {
     const businessArray = useBusinesses();
 
     const newYorkBusinessHTML = "<h1>Active Businesses</h1>";
     businessArray.forEach(
         (businessObject) => {
-            contentTarget.innerHTML += Business(businessObject)
+            contentTarget.innerHTML += businessHTML(businessObject)
         }
     );
     newYorkBusinessHTML.innerHTML += `
